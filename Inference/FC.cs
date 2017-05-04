ca852f153837df11f21418d4aafbd75cdbaa0d02
James Crawford <jimmyson.2007@gmail.com>
2017-05-04 15:13:47 +1000

    Note for Forward Chain

---
+++ Inference/FC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inference
+{
+    /*
+     * Read all root nodes, and prints full node path to destintion if found.
+     * eg: a, b, p1, p2, p3, d
+     */
+    class FC
+    {
+    }
+}
