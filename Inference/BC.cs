04dd04f12500d0c9088f3d60be459550e1fd3e1e
James Crawford <jimmyson.2007@gmail.com>
2017-05-04 15:16:06 +1000

    Note for BC

---
+++ Inference/BC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inference
+{
+    /*
+     * Return path of node chain, reading from destination. Does not print other root nodes.
+     * Checks for loops.
+     * eg: p1, p2, p3, d
+     */
+    class BC
+    {
+    }
+}
