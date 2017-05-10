852ecbd29fe26d341cae5bd1f31354fbef39a2f5
CalvinWLiem <calvinwliem@gmail.com>
2017-05-10 17:23:34 +1000

    Revert "Comments and fixing up code."
    This reverts commit 241103c9eb7a8011cdd4bc3923490c9a1a1f4d27.

--- Inference/Search.cs
+++ Inference/Search.cs
@@ -1,31 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 
 namespace Inference
 {
     abstract class Search
     {
-        List<string> agenda; // Initial Variables (i.e. A;)  
+        abstract List<string> agenda;
 
-      // List<string> facts; 
-        List<string> clauses;  // Sentences (i.e. A => B or A & C => B)
-        List<string> entailed; // Response (taken from the agenda)
-        List<int> count; // Count the conjunctions.
+        abstract List<string> facts;
+        abstract List<string> clauses;
+        abstract List<string> entailed;
+        abstract List<int> count;
 
-        abstract public void Process();    // Implementation of the respective algorithm's output
-        abstract public bool Algorithm(); // Implementation of the respective algorithm
-
-        public bool PremiseContains(string clause, string p)
-        {
-            // Check the conjuncts in the clause.
-            // If only 1, check the conjuct against p
-            // If multiple, check each of them against p
-            return false;
-        }
-        public void BuildKB(string tell)
-        {
-            //Look for Inference
-                //If true, add to clause and count the conjuctions (A&B or Z)
-                //If false, add to agenda
-        }
+        abstract void Process();
     }
 }
