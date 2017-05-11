ecdf42542196d26e5a6665f45c63586491d6bce4
James Crawford <jimmyson.2007@gmail.com>
2017-05-11 15:11:37 +1000

    Revisions to Search. Abstracting BuildKB and Renaming PremiseContains for a more universal function

--- Inference/Search.cs
+++ Inference/Search.cs
@@ -14,45 +14,11 @@ namespace Inference
 
         abstract public void Process(string tell);    // Implementation of the respective algorithm's output
         abstract public bool Algorithm(); // Implementation of the respective algorithm
+        abstract public void BuildKB(string tell);  // Build the Facts and Clauses 
 
-        public bool PremiseContains(string clause, string p)
-        {
-            // Get the premise by spliting the clause and get the first element.
-            string premise = clause.Split(new[] { "=>" }, System.StringSplitOptions.None)[0];
-            // Get the conjucts by spliting premise
-            List<string> conjucts = premise.Split('&').ToList();
-
-            // Check the conjunctions in the clause.
-            if (conjucts.Count == 1)
-            {
-                // If only 1, check the conjuct against p
-                return premise.Equals(p);
-            }
-            else
-            {
-                // If multiple, check each of them against p
-                return conjucts.Contains(p);
-            }
-        }
-        public void BuildKB(string tell)
-        {
-            // Split tell into list of strings
-            List<string> sentences = tell.Split(';').ToList();
-            for (int i=0; i<sentences.Count;i++)
-            {
-                //Look for Inference
-                if (sentences[i].Contains("=>"))
-                {
-                    //If true, add to clause and count the conjuctions (A&B or Z)
-                    Clauses.Add(sentences[i]);
-                    Count.Add(sentences[i].Split('&').Length);
-                }
-                else
-                {
-                    //If false, add to agenda
-                    Agenda.Add(sentences[i]);
-                }
-            }
-        }
+        /**
+         * Looks for a variable. Depends on Chaining Method.
+         */
+        abstract public bool Contains(string clause, string search);    //Search for the variable;
     }
 }
