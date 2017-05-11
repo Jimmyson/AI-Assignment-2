029907cc686b6b8230bea0082433eefcec67793c
CalvinWLiem <calvinwliem@gmail.com>
2017-05-11 15:11:16 +1000

    Attempt Fixing Rebase Issues

--- Inference/FC.cs
+++ Inference/FC.cs
@@ -11,39 +11,8 @@ namespace Inference
      */
     class FC
     {
-        override process (kb,q )
-            //kb = tell
-            //q = ask
-        {
-            count // A table indexed by clause, initially number of premises
-            inferred // A table indexed by symbol, initiated with false
-            agenda // a list of symbols, initially known to be true
-
-            while (agenda.!empty())
-                {
-                    p = pop agenda
-                    if (!inferred(p))
-                {
-                    for (each kb c in premise p appears)
-                    {
-                        if count[c] =
-                            if Head[c] = Queryable then return true
-                                push(head[c].agenda)
-                    }
-                            }
-                }
-
-            return false;
-        }
+        
     }
 }
-
-abstract List<string> agenda;
-
-abstract List<string> facts;
-abstract List<string> clauses;
-abstract List<string> entailed;
-abstract List<int> count;
-
 
 
